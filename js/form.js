d01e71ac20cb4c70544f72c54f5aec973f0ae4da
Bruno-Goudric <brunolfsports@hotmail.com>
2019-01-10 19:26:12 -0200

    validando o restante dos campos do formulário

--- js/form.js
+++ js/form.js
@@ -11,9 +11,10 @@ botaoAdicionar.addEventListener("click", function(event) {
 
     var erros = validaPaciente(paciente);
 
+    console.log(erros);
+
     if (erros.length > 0) {
-        var mensagemErro = document.querySelector("#mensagem-erro");
-        mensagemErro.textContent = erros;
+        exibeMensagensDeErro(erros);
         return;
     }
 
@@ -24,7 +25,21 @@ botaoAdicionar.addEventListener("click", function(event) {
 
     form.reset();
 
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
+
 });
+
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
+
 
 function obtemPacienteDoFormulario(form){
         //criando um objeto paciente
@@ -82,11 +97,27 @@ function validaPaciente(paciente){
     var erros = [];
 
     if(!validaPeso(paciente.peso)){
-        erro.push("Peso é inválido");
+        erros.push("Peso é inválido");
     }
 
     if(!validaAltura(paciente.altura)){
-        erro.push("Altura é inválida");
+        erros.push("Altura é inválida");
+    }
+
+    if(paciente.nome.length == 0){
+        erros.push("O nome não pode ser em branco");
+    }
+
+    if(paciente.peso.length == 0){
+        erros.push("O peso não pode estar em branco");
+    }
+
+    if(paciente.altura.length == 0){
+        erros.push("A altura não pode estar em branco");
+    }
+
+    if(paciente.gordura.length == 0){
+        erros.push("A gordura não pode ser em branco");
     }
 
     return erros;
